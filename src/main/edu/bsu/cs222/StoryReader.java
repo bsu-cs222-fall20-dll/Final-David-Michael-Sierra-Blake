727da752c52cff6ee4230a43c4e3515852397ea2
Michael Heckman <msheckman@bsu.edu>
2020-10-26 15:14:47 -0400

    Renamed Reader to StoryReader, moved to main

---
+++ src/main/edu/bsu/cs222/StoryReader.java
@@ -0,0 +1,18 @@
+package edu.bsu.cs222;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class StoryReader {
+    public JsonObject parse(InputStream input) {
+        JsonParser parser = new JsonParser();
+        Reader reader = new InputStreamReader(input);
+        JsonElement rootElement = parser.parse(reader);
+        return rootElement.getAsJsonObject();
+    }
+}
