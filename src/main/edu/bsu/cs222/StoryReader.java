573b0a0bf62db79d23603355371e858a27c94bac
Michael Heckman <msheckman@bsu.edu>
2020-10-26 21:46:05 -0400

    More tests in ReaderTest, added functions in StoryReader

--- src/main/edu/bsu/cs222/StoryReader.java
+++ src/main/edu/bsu/cs222/StoryReader.java
@@ -3,6 +3,7 @@ package edu.bsu.cs222;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,4 +16,18 @@ public class StoryReader {
         JsonElement rootElement = parser.parse(reader);
         return rootElement.getAsJsonObject();
     }
+
+    public JsonObject roomReceiver(JsonObject rootObject) {
+        JsonObject room = rootObject.getAsJsonObject("TestStory").getAsJsonObject("StartRoom");
+        System.out.println(room);
+        return room;
+    }
+
+    public JsonPrimitive textReceiver(JsonObject room) {
+        return room.getAsJsonPrimitive("Text");
+    }
+
+    public JsonObject enemyReceiver() {return null;}
+
+
 }
--- src/test/edu/bsu/cs222/ReaderTest.java
+++ src/test/edu/bsu/cs222/ReaderTest.java
@@ -1,9 +1,9 @@
 package edu.bsu.cs222;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -11,26 +11,47 @@ import java.io.*;
 public class ReaderTest {
 
     @Test
-    public void testPrints() throws FileNotFoundException {
+    public void testPrintsStartRoom() throws FileNotFoundException {
         StoryReader storyReader = new StoryReader();
 
         InputStream is = new FileInputStream("src/test/resources/test-story.json");
         JsonObject rootObject = storyReader.parse(is);
         JsonObject startRoom = rootObject.getAsJsonObject("TestStory").getAsJsonObject("StartRoom");
-        JsonObject roomTwo = rootObject.getAsJsonObject("TestStory").getAsJsonObject("RoomTwo");
-        JsonObject roomThree = rootObject.getAsJsonObject("TestStory").getAsJsonObject("RoomThree");
-        JsonObject roomFour = rootObject.getAsJsonObject("TestStory").getAsJsonObject("RoomFour");
 
         JsonPrimitive startRoomText = startRoom.getAsJsonPrimitive("Text");
         JsonObject startRoomActions = startRoom.getAsJsonObject("Actions");
         JsonArray startRoomActions1 = startRoomActions.getAsJsonArray("Action1");
         JsonArray startRoomActions2 = startRoomActions.getAsJsonArray("Action2");
         JsonArray startRoomActions3 = startRoomActions.getAsJsonArray("Action3");
+        JsonArray startRoomPuzzle = startRoom.getAsJsonArray("Puzzle");
+        JsonArray startRoomEnemies = startRoom.getAsJsonArray("Enemies");
+        JsonPrimitive startRoomEnemyClear = startRoom.getAsJsonPrimitive("EnemyClear");
 
         System.out.println(startRoomText);
         System.out.println(startRoomActions1);
         System.out.println(startRoomActions2);
         System.out.println(startRoomActions3);
+        System.out.println(startRoomPuzzle);
+        System.out.println(startRoomEnemies);
+        System.out.println(startRoomEnemyClear);
+    }
 
+    @Test
+    public void roomReceiverTest() throws FileNotFoundException {
+        StoryReader storyReader = new StoryReader();
+        InputStream is = new FileInputStream("src/test/resources/test-story.json");
+        JsonObject rootObject = storyReader.parse(is);
+        JsonObject room = storyReader.roomReceiver(rootObject);
+        Assertions.assertEquals(room, "{\"Text\":\"This is the start room to the test story.\",\"Actions\":{\"Action1\":[\"This is the first action.\",\"RoomTwo\"],\"Action2\":[\"This is the second action.\",\"RoomThree\"],\"Action3\":[\"This is the third action. It will end the game.\",\"End\"]},\"Puzzle\":[\"null\",\"ifPass\",\"ifFail\"],\"Enemies\":[],\"EnemyClear\":\"null\"}\n");
+    }
+
+    @Test
+    public void textReceiverTest() throws FileNotFoundException {
+        StoryReader storyReader = new StoryReader();
+        InputStream is = new FileInputStream("src/test/resources/test-story.json");
+        JsonObject rootObject = storyReader.parse(is);
+        JsonObject room = storyReader.roomReceiver(rootObject);
+        JsonPrimitive text = storyReader.textReceiver(room);
+        Assertions.assertEquals(text, "This is the start room to the test story.");
     }
 }
