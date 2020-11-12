a666e7e11183c1be1f6067066dd7da24e926bfa0
Michael Heckman <msheckman@bsu.edu>
2020-11-12 16:39:19 -0500

    Fixed RoomBuilder to work with new Room class, reformatted StoryTest to work with fixed RoomBuilder

--- src/main/edu/bsu/cs222/RoomBuilder.java
+++ src/main/edu/bsu/cs222/RoomBuilder.java
@@ -1,94 +1,13 @@
 package edu.bsu.cs222;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+
+import java.io.FileNotFoundException;
 
 
 public class RoomBuilder {
-    StoryReader storyReader = new StoryReader();
-
-    public JsonObject firstRoom(JsonObject story, String storyName) {
-        JsonObject currentRoom = storyReader.roomReceiver(story, "StartRoom", storyName);
-
-        return storyReader.actionsReceiver(currentRoom);
-
-
+    public Room nextRoom(JsonObject story, String roomName, String storyName) throws FileNotFoundException {
+        return new Room(story, roomName, storyName);
     }
-
-    public JsonObject nextRoom(JsonObject story, String roomName, String storyName) {
-        JsonObject currentRoom = storyReader.roomReceiver(story, roomName, storyName);
-
-        if (roomName.equals("\"LastRoom\"")) {
-            lastRoom();
-        } else if (!storyReader.puzzleReceiver(currentRoom).toString().equals("[\"null\", \"ifPass\", \"ifFail\"]")){
-            return puzzleRoomInitiate(story, roomName, storyName);
-        } else if (!storyReader.enemyReceiver(currentRoom).toString().equals("[]")) {
-            enemyRoom(story, roomName, storyName);
-        } else if (roomName.equals("\"EndWin\"")) {
-            winRoom(story, roomName, storyName);
-        } else if (roomName.equals("\"EndLose\"")) {
-            loseRoom(story, roomName, storyName);
-        } else {
-            emptyRoom();
-        }
-        return null;
-    }
-
-    public JsonPrimitive enemyRoom(JsonObject story, String roomName, String storyName) {
-        System.out.println("Enemy Room!");
-        JsonObject currentRoom = storyReader.roomReceiver(story, roomName, storyName);
-
-        return storyReader.enemyClear(currentRoom);
-
-    }
-
-    public JsonObject puzzleRoomInitiate(JsonObject story, String roomName, String storyName) {
-        System.out.println("Puzzle room!");
-
-        JsonObject currentRoom = storyReader.roomReceiver(story, roomName, storyName);
-
-        return storyReader.actionsReceiver(currentRoom);
-    }
-
-    public String puzzleRoomAction(JsonObject story, String roomName, String actionChoice, String storyName) {
-        if(actionChoice.equals("DoPuzzle")) {
-            return puzzleRoom(story, roomName, storyName);
-        } else if (actionChoice.equals("IgnorePuzzle")) {
-            loseRoom(story, roomName, storyName);
-        }
-        return null;
-    }
-
-    public String puzzleRoom(JsonObject story, String roomName, String storyName) {
-        System.out.println("Doing the puzzle!");
-        JsonObject currentRoom = storyReader.roomReceiver(story, roomName, storyName);
-
-        JsonArray puzzleArray = storyReader.puzzleReceiver(currentRoom);
-
-        Puzzle puzzle = new Puzzle();
-        return puzzle.ifPassAction(puzzleArray);
-
-    }
-
-    public void emptyRoom() {
-    }
-
-    public void lastRoom() {
-
-    }
-
-    public void loseRoom(JsonObject story, String roomName, String storyName) {
-        JsonObject currentRoom = storyReader.roomReceiver(story, roomName, storyName);
-        String roomText = storyReader.textReceiver(currentRoom);
-        System.out.println(roomText);
-    }
-
-    public void winRoom(JsonObject story, String roomName, String storyName) {
-        JsonObject currentRoom = storyReader.roomReceiver(story, roomName, storyName);
-        String roomText = storyReader.textReceiver(currentRoom);
-        System.out.println(roomText);
-    }
-
 
 }
--- src/test/edu/bsu/cs222/StoryTest.java
+++ src/test/edu/bsu/cs222/StoryTest.java
@@ -1,13 +1,11 @@
 package edu.bsu.cs222;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class StoryTest {
 
@@ -17,22 +15,18 @@ public class StoryTest {
         RoomBuilder roomBuilder = new RoomBuilder();
 
         JsonObject story = storyReader.parse(new FileInputStream("src/test/resources/test-story.json"));
-        String storyName = storyReader.getStoryName(new FileInputStream("src/test/resources/test-story.json"));
+        String storyName = "TestStory";
+        String roomName = "StartRoom";
 
-        JsonObject firstRoomActions = roomBuilder.firstRoom(story, storyName); // Room 1
-
-        ArrayList<JsonArray> actionList = storyReader.getActionList(firstRoomActions);
-        String actionResult = storyReader.getActionListResult(actionList.get(0)); // First action on actionList for StartRoom
-
-        JsonObject puzzleActions = roomBuilder.nextRoom(story, actionResult, storyName); // Room 2
-
-        ArrayList<JsonArray> puzzleActionList = storyReader.getActionList(puzzleActions);
-        String puzzleActionResult = storyReader.getActionListResult(puzzleActionList.get(0));
-        String nextRoom = roomBuilder.puzzleRoomAction(story, actionResult, puzzleActionResult, storyName);
-
-        roomBuilder.nextRoom(story, nextRoom, storyName); // Room 4
-
-
+        Room startRoom = roomBuilder.nextRoom(story, roomName, storyName); //Room 1
+        String nextRoomName = startRoom.getActionResult(0); //Action 1: "Go to Room 2"
+        Room nextRoom = roomBuilder.nextRoom(story, nextRoomName, storyName); //Room 2
+        Puzzle puzzle = nextRoom.getPuzzle();
+        String ifPassAction = puzzle.getIfPassAction(); //Action on pass: "Go to Room 4"
+        nextRoom = roomBuilder.nextRoom(story, ifPassAction, storyName); // Room 4
+        nextRoomName = nextRoom.getActionResult(0); //Action 1: "Go to EndWin"
+        nextRoom =  roomBuilder.nextRoom(story, nextRoomName, storyName); // EndWin
+        Assertions.assertEquals(nextRoom.getRoomText(),"You have won the test story.");
     }
 
     @Test
@@ -41,18 +35,16 @@ public class StoryTest {
         RoomBuilder roomBuilder = new RoomBuilder();
 
         JsonObject story = storyReader.parse(new FileInputStream("src/test/resources/test-story.json"));
-        String storyName = storyReader.getStoryName(new FileInputStream("src/test/resources/test-story.json"));
+        String storyName = "TestStory";
+        String roomName = "StartRoom";
 
-        JsonObject firstRoomActions = roomBuilder.firstRoom(story, storyName); // Room 1
-
-        ArrayList<JsonArray> actionList = storyReader.getActionList(firstRoomActions);
-        String actionResult = storyReader.getActionListResult(actionList.get(0)); // First action on actionList for StartRoom
-
-        JsonObject puzzleActions = roomBuilder.nextRoom(story, actionResult, storyName); // Room 2
-
-        ArrayList<JsonArray> puzzleActionList = storyReader.getActionList(puzzleActions);
-        String puzzleActionResult = storyReader.getActionListResult(puzzleActionList.get(1));
-        roomBuilder.puzzleRoomAction(story, actionResult, puzzleActionResult, storyName);
+        Room startRoom = roomBuilder.nextRoom(story, roomName, storyName); //Room 1
+        String nextRoomName = startRoom.getActionResult(0); //Action 1: "Go to Room 2"
+        Room nextRoom = roomBuilder.nextRoom(story, nextRoomName, storyName); //Room 2
+        Puzzle puzzle = nextRoom.getPuzzle();
+        String ifFailAction = puzzle.getIfFailAction(); //Action on fail: "Go to EndLose"
+        nextRoom = roomBuilder.nextRoom(story, ifFailAction, storyName); //EndLose
+        Assertions.assertEquals(nextRoom.getRoomText(),"You have lost the test story.");
     }
 
     @Test
@@ -60,16 +52,17 @@ public class StoryTest {
         StoryReader storyReader = new StoryReader();
         RoomBuilder roomBuilder = new RoomBuilder();
 
-        String storyName = storyReader.getStoryName(new FileInputStream("src/test/resources/test-story.json"));
         JsonObject story = storyReader.parse(new FileInputStream("src/test/resources/test-story.json"));
+        String storyName = "TestStory";
+        String roomName = "StartRoom";
 
-
-        JsonObject firstRoomActions = roomBuilder.firstRoom(story, storyName); // Room 1
-
-        ArrayList<JsonArray> actionList = storyReader.getActionList(firstRoomActions);
-        String actionResult = storyReader.getActionListResult(actionList.get(1)); // First action on actionList for StartRoom
-
-        JsonPrimitive enemyClear = roomBuilder.enemyRoom(story, actionResult, storyName);
-        roomBuilder.nextRoom(story, enemyClear.toString(), storyName);
+        Room startRoom = roomBuilder.nextRoom(story, roomName, storyName); //Room 1
+        String nextRoomName = startRoom.getActionResult(1); //Action 1: "Go to Room 3"
+        Room nextRoom = roomBuilder.nextRoom(story, nextRoomName, storyName); //Room 3
+        String enemyClear = nextRoom.getEnemyClear(); //Action: "Go to Room 4"
+        nextRoom = roomBuilder.nextRoom(story, enemyClear, storyName); // Room 4
+        nextRoomName = nextRoom.getActionResult(0); //Action 1: "Go to EndWin"
+        nextRoom =  roomBuilder.nextRoom(story, nextRoomName, storyName); // EndWin
+        Assertions.assertEquals(nextRoom.getRoomText(),"You have won the test story.");
     }
 }
