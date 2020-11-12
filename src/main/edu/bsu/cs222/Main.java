44910afb75730bcaa15fea3e06d7f7f74b8239ef
Michael Heckman <msheckman@bsu.edu>
2020-11-12 16:08:10 -0500

    More setup for Main.java

--- src/main/edu/bsu/cs222/Main.java
+++ src/main/edu/bsu/cs222/Main.java
@@ -1,9 +1,13 @@
 package edu.bsu.cs222;
+import com.google.gson.JsonObject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Main extends Application{
     @Override
@@ -12,6 +16,27 @@ public class Main extends Application{
         primaryStage.setTitle("Game Title");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        StoryReader storyReader = new StoryReader();
+        RoomBuilder roomBuilder = new RoomBuilder();
+        InputStream is = new FileInputStream("src/test/resources/test-story.json");
+
+        JsonObject storyObject = storyReader.parse(is);
+        String storyName = "TestStory";
+        String roomName = "StartRoom";
+
+        while(true) {
+            Room room = roomBuilder.nextRoom(storyObject, roomName, storyName);
+            String boxinfo = room.getRoomText();
+            String actionbox1title = room.getAction(0);
+            String actionbox2title = room.getAction(1);
+            String actionbox3title = room.getAction(2);
+            String actionbox1onclick = room.getActionResult(0);
+            String actionbox2onclick = room.getActionResult(1);
+            String actionbox3onclick = room.getActionResult(2);
+
+            break;
+        }
     }
 
     public static void main(String[] args) { launch(args); }
