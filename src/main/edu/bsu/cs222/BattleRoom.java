5aeee628de5785d7826f47d5b343277b7bab5101
Michael Heckman <msheckman@bsu.edu>
2020-11-16 22:20:45 -0500

    Finished BattleRoom for Iteration 2

--- src/main/edu/bsu/cs222/BattleRoom.java
+++ src/main/edu/bsu/cs222/BattleRoom.java
@@ -1,39 +1,35 @@
 package edu.bsu.cs222;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Random;
 public class BattleRoom extends Battle {
 
-    Player player=new Player();
-    Enemy enemy=BattleRoom.getEnemy(0);
 
-    public BattleRoom() throws FileNotFoundException {
+    public String battle(Room room) throws FileNotFoundException {
         int dice;
-        int health;
-        dice = (int)(Math.random()*6) + 1;
-        health = battleRoom.healthReceiver(enemyObject);
-        if (dice >= getHit(1)){
-            health == health-1;
-            System.out.println("You rolled a " + dice + ". You hit. Enemy health =" + health);
+        Player player = new Player();
 
+        ArrayList<Enemy> enemyArrayList = room.getEnemies();
+
+        for(Enemy enemy: enemyArrayList) {
+            while(enemy.getEnemyHealth() > 0) {
+                dice = (int) (Math.random() * 6) + 1;
+                if (dice > enemy.getEnemyHit()) {
+                    enemy.setEnemyHealth(1);
+                    System.out.println("You rolled a " + dice + ". You hit. Enemy health = " + enemy.getEnemyHealth());
+                } else if (dice == enemy.getEnemyHit()) {
+                    System.out.println("You did not hit. Enemy health = " + enemy.getEnemyHealth());
+                } else {
+                    player.setHealthAfterDamage(1);
+                    System.out.println("You were hit by the enemy for 1 damage. Current health = " + player.getHealth());
+                }
+                if(player.getHealth() == 0) {
+                    System.out.println("You have lost all of your health!");
+                    return "EndLose";
+                }
+            }
         }
-        else
-            System.out.println("You did not hit. Enemy health =" + health);
-
+        return room.getEnemyClear();
     }
-
-
-    //TODO: create a variable for the player that is a number between 1 and 6
-    //TODO: grab the Player class with something like Player player = new Player();
-    //TODO: call the current room in the class (require it in the parameter)
-    //TODO: grab the enemy with something like Enemy enemy = room.getEnemy(0); This would be the first enemy in the room
-    //Whenever the roll variable is higher than the hit value of the enemy's enemy.getEnemyHit(),
-    //decide the damage however you need, and then call enemy.setEnemyHealth(damage) to subtract the damage from their health (damage should be a positive int)
-    //Whenever the roll variable is lower than the enemy's hit value, call the player's player.setHealthAfterDamage(damage) (damage should be a positive int)
-    //This will continue until either the player is dead, or the enemy is dead
-
-    //Eventually:
-    //When the player dies, this will return the string "EndLose"
-    //When the player wins, this will return the string stored in currentRoom.getEnemyClear()
-    //If there is more than one enemy, the battle will be in a loop until the player dies or runs out of enemies to kill and wins
 }
