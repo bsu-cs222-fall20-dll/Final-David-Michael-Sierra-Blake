c7a109217cf1261b0600aecbdf00f59715963912
Michael Heckman <msheckman@bsu.edu>
2020-12-03 17:27:25 -0500

    Added GUI integration

--- src/main/java/edu/bsu/cs222/BattleRoom.java
+++ src/main/java/edu/bsu/cs222/BattleRoom.java
@@ -1,13 +1,19 @@
 package edu.bsu.cs222;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
 public class BattleRoom extends Battle {
 
 
-    public String battle(Room room) throws FileNotFoundException {
+    public String battle(Room room, Button actionButton1, TextArea roomText) throws FileNotFoundException {
         System.out.println();
         System.out.println();
+
+        StringBuilder battleText = new StringBuilder();
 
         int dice;
         Player player = new Player();
@@ -16,26 +22,30 @@ public class BattleRoom extends Battle {
 
         for(Enemy enemy: enemyArrayList) {
             System.out.println();
-            System.out.println("Enemy: " + enemy.getEnemyName());
+            battleText.append("Enemy: ").append(enemy.getEnemyName());
             while(enemy.getEnemyHealth() > 0) {
                 dice = (int) (Math.random() * 6) + 1;
                 if (dice > enemy.getEnemyHit()) {
                     enemy.setEnemyHealth(1);
-                    System.out.println("You rolled a " + dice + ". You hit. Enemy health = " + enemy.getEnemyHealth());
+                    battleText.append("\nYou rolled a ").append(dice).append(". You hit. Enemy health = ").append(enemy.getEnemyHealth());
                 } else if (dice == enemy.getEnemyHit()) {
-                    System.out.println("You did not hit. Enemy health = " + enemy.getEnemyHealth());
+                    battleText.append("\nYou did not hit. Enemy health = ").append(enemy.getEnemyHealth());
                 } else {
                     player.setHealthAfterDamage(1);
-                    System.out.println("You were hit by the enemy for 1 damage. Current health = " + player.getHealth());
+                    battleText.append("\nYou were hit by the enemy for 1 damage. Current health = ").append(player.getHealth());
                 }
                 if(player.getHealth() == 0) {
-                    System.out.println("You have lost all of your health!");
+                    battleText.append("\nYou have lost all of your health!");
+                    roomText.setText(battleText.toString());
+                    actionButton1.setText("End Game");
                     return "EndLose";
                 }
             }
         }
+        roomText.setText(battleText.toString());
         System.out.println();
         System.out.println();
+        actionButton1.setText("Continue to next room");
         return room.getEnemyClear();
     }
 }
