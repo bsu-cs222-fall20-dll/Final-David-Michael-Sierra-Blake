0a8091b3a1c1265bf1ff597adb1daad9bd9e5d36
Michael Heckman <msheckman@bsu.edu>
2020-12-03 17:24:59 -0500

    Edited main to start GUI class

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -5,41 +5,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 import java.awt.*;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/MainGUI.fxml"));
-        primaryStage.setTitle("Game Title");
-        primaryStage.setScene(new Scene(root, 600, 500));
-        primaryStage.show();
+        String storyName = "WaterStory";
+        String roomName = "StartRoom";
 
         StoryReader storyReader = new StoryReader();
         RoomBuilder roomBuilder = new RoomBuilder();
-        InputStream is = new FileInputStream("src/test/resources/test-story.json");
+        InputStream is = new FileInputStream("src/main/resources/water-story.json");
 
         JsonObject storyObject = storyReader.parse(is);
-        String storyName = "TestStory";
-        String roomName = "StartRoom";
-        TextArea currentSituationText;
 
-        while (true) {
-            Room room = roomBuilder.nextRoom(storyObject, roomName, storyName);
-            String boxinfo = room.getRoomText();
-            String action1 = room.getAction(0);
-            String action2 = room.getAction(1);
-            String action3 = room.getAction(2);
-            String action1onclick = room.getActionResult(0);
-            String action2onclick = room.getActionResult(1);
-            String action3onclick = room.getActionResult(2);
+        GUI gui = new GUI(primaryStage, storyName, roomName, storyObject);
 
-            break;
-        }
+        gui.setRoom(roomBuilder.nextRoom(storyObject, roomName, storyName));
+
     }
 }
