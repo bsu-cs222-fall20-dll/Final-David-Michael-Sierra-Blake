750f94c7febc0a01762386f477afb641586db17c
Michael Heckman <msheckman@bsu.edu>
2020-12-03 17:26:08 -0500

    Removed Controller from usability

--- src/main/java/edu/bsu/cs222/Controller.java
+++ src/main/java/edu/bsu/cs222/Controller.java
@@ -2,6 +2,7 @@ package edu.bsu.cs222;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
 public class Controller {
@@ -10,8 +11,13 @@ public class Controller {
     public TextArea currentActionsField;
     public TextArea statsField;
     public TextArea skillPointField;
+    public Button actionButton1;
+    public Button actionButton2;
+    public Button actionButton3;
+
 
     public void doAction3(javafx.event.ActionEvent actionEvent) {
+        System.out.println("Action!");
     }
 
     public void doAction2(javafx.event.ActionEvent actionEvent) {
@@ -21,5 +27,9 @@ public class Controller {
     }
 
     public void displayMenu(ActionEvent actionEvent) {
+    }
+
+    public void update(Room room) {
+        actionButton3.setText(room.getAction(2));
     }
 }
