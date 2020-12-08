39048c4200a2fcf9d4805010fb58f1b28ba12d19
Michael Heckman <msheckman@bsu.edu>
2020-12-08 17:14:58 -0500

    Finished cleaning Main class

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,15 +1,11 @@
 package edu.bsu.cs222;
-import com.google.gson.JsonObject;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         MainMenu mainMenu = new MainMenu(primaryStage);
 
     }
